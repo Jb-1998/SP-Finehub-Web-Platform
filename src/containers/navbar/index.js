aac583da7bae53b8fb86e5b5c7b4242cc2ae3325
ellineison <30744570+ellineison@users.noreply.github.com>
2021-04-21 08:07:52 +0800

    hamburger menu (1)

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -62,12 +62,14 @@ const NavigationBar = () => {
 
  export default NavigationBar;
 
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
+ const hamburger = document.querySelector(".hamburger");
+ const navMenu = document.querySelector(".nav-menu");
 
-  hamburger.addEventListener("click", mobileMenu);
+ if(hamburger){
+   hamburger.addEventListener("click", mobileMenu, true);
+ }
 
-  function mobileMenu() {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-  }
+ function mobileMenu() {
+       hamburger.classList.toggle("active");
+       navMenu.classList.toggle("active");
+   }

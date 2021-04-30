916b327f51667dd9f3322b8c0ebe921b05d9f406
ellineison <30744570+ellineison@users.noreply.github.com>
2021-04-30 16:08:53 +0800

    3 graphs in emotion page

--- src/pages/emotion/index.js
+++ src/pages/emotion/index.js
@@ -58,14 +58,105 @@ import { ReactComponent as ShareIcon } from '../../assets/icons/share.svg';
 import {Link} from '@reach/router';
 
 // charts
+import { Line, Bar, Radar } from 'react-chartjs-2';
 
 // MAIN SCREEN COMPONENTS
+const data1 = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "Sadness",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,  192,192,1)"
+    },
+    {
+      label: "Anxiety",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: false,
+      borderColor: "#742774"
+    }
+  ]
+};
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+const data2 = {
+  labels: labels,
+  datasets: [{
+    axis: 'y',
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
 
+const data3 = {
+  labels: [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 90, 81, 56, 55, 40],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [28, 48, 40, 19, 96, 27, 100],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
 
 const Emotion = () => {
 
   return (
-      <h1> hi </h1>
+<body>
+    <div>
+      <Line className="linechart" data={data1} />
+    </div>
+    <div>
+      <Bar className="barchart" data={data2} />
+    </div>
+    <div>
+      <Radar className="radarchart" data={data3} />
+    </div>
+</body>
+
   )
 }
 
